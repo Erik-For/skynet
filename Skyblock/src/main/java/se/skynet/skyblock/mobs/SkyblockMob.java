9e609d2ce95386b00ae02d363e6215a0aeff2f32
x6033 <x603.ef@gmail.com>
2025-04-29 17:18:48 +0200

    dev

--- Skyblock/src/main/java/se/skynet/skyblock/mobs/SkyblockMob.java
+++ Skyblock/src/main/java/se/skynet/skyblock/mobs/SkyblockMob.java
@@ -58,9 +58,9 @@ public class SkyblockMob {
         /* TODO make this work instead of customName because it is visible father back */
         int hologram = HologramManager.createHologram(entity.getLocation().add(0, 4, 0), customNameFormatted(level, name, health));
         ArmorStand hologramEntity = (ArmorStand) entity.getWorld().getEntities().stream().filter(e -> e.getEntityId() == hologram).findFirst().orElse(null);
-        System.out.println(hologramEntity.getType());
         if (hologramEntity != null) {
             hologramEntity.setMarker(false);
+            hologramEntity.setSmall(false);
             entity.setPassenger(hologramEntity);
         }
 
